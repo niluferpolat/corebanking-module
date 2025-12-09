e5ff1dad261e27cc1425851c414b24d98fbb33bc
nilufer polat <niluferpolat2@gmail.com>
2025-12-09 16:46:44 +0300

    first commit

---
+++ src/test/java/com/nilufer/minibank/MinibankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nilufer.minibank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinibankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
